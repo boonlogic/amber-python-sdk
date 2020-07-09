9257c926b7474b93f4f83234f1a11f460f190fae
Jim Turnquist <jim@boonlogic.com>
2020-07-09 07:09:47 -0500

    Add basic Makefile in preparation for pipeline build

--- setup.py
+++ setup.py
@@ -5,16 +5,16 @@ with open("README.md", "r") as fh:
 
 setup(
     name='boonamber',
-    version='1.0',
+    version='1.0.1',
     author="BoonLogic",
-    author_email="luke@boonlogic.com",
+    author_email="amber-support@boonlogic.com",
     packages=['boonamber'],
     install_requires=['requests'],
     description="An SDK for Boon Amber sensor analytics",
     long_description=long_description,
     license='MIT',
     long_description_content_type="text/markdown",
-    url=None,
+    url="https://github.com/boonlogic/amber-python-sdk",
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
