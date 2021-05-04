ec4e756b1e61d1322fd2d2956b0d9242a538a545
Jim Turnquist <jim@boonlogic.com>
2021-05-04 21:23:23 +0000

    Update to version 14 and create new docs

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='boonamber',
-    version='1.0.13',
+    version='1.0.14',
     author="BoonLogic",
     author_email="amber-support@boonlogic.com",
     packages=['boonamber'],
