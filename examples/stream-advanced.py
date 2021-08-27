1c4b01720571caf1b910adf0a112a4cfcb103642
Jim Turnquist <jim@boonlogic.com>
2021-08-27 09:27:41 -0500

    Update stream-advanced.py to handle configuration and fusion data better.

--- examples/stream-advanced.py
+++ examples/stream-advanced.py
@@ -10,27 +10,41 @@ from boonamber import AmberClient, AmberCloudError
 
 class AmberStream:
 
-    def __init__(self, sensor_id=None):
+    def __init__(self, sensor_id=None, label=None, feature_count=1, streaming_window_size=25, samples_to_buffer=10000,
+                 anomaly_history_window=10000, learning_rate_numerator=10, learning_rate_denominator=10000,
+                 learning_max_clusters=1000, learning_max_samples=1000000):
         """
         Initializes the AmberStream example class.
         :param sensor_id: The sensor_id to be used by AmberStream.  If sensor_id is None, then a sensor is created
         """
         self.data = []
         self.sample_cnt = 0
+        self.sensor_id = sensor_id
+        self.label = label
+        self.feature_count = feature_count
+        self.streaming_windows_size = streaming_window_size
+        self.samples_to_buffer = samples_to_buffer
+        self.anomaly_history_window = anomaly_history_window
+        self.learning_max_clusters = learning_max_clusters
+        self.learning_max_samples = learning_max_samples
+        self.learning_rate_numerator = learning_rate_numerator
+        self.learning_rate_denominator = learning_rate_denominator
 
         try:
             self.amber = AmberClient()
             if sensor_id is None:
-                self.sensor_id = self.amber.create_sensor(label='stream-example-sensor')
-                print("created sensor {}".format(sensor_id))
+                self.sensor_id = self.amber.create_sensor(label=self.label)
+                print("created sensor {}".format(self.sensor_id))
             else:
-                self.sensor_id = sensor_id
-                print("using sensor {}".format(sensor_id))
+                print("using sensor {}".format(self.sensor_id))
 
-            config = self.amber.configure_sensor(self.sensor_id, feature_count=1, streaming_window_size=25,
-                                                 samples_to_buffer=1000, learning_max_clusters=1000,
-                                                 learning_max_samples=20000, learning_rate_numerator=0,
-                                                 learning_rate_denominator=20000)
+            config = self.amber.configure_sensor(self.sensor_id, feature_count=self.feature_count,
+                                                 streaming_window_size=self.streaming_windows_size,
+                                                 samples_to_buffer=self.samples_to_buffer,
+                                                 learning_max_clusters=self.learning_max_clusters,
+                                                 learning_max_samples=self.learning_max_samples,
+                                                 learning_rate_numerator=self.learning_max_samples,
+                                                 learning_rate_denominator=self.learning_rate_denominator)
             print("{} config: {}".format(self.sensor_id, config))
         except AmberCloudError as e:
             print(e)
@@ -49,7 +63,8 @@ class AmberStream:
         print("State: {}({}%), inferences: {}, clusters: {}, samples: {}, duration: {}".format(
             results['state'], results['progress'], results['totalInferences'], results['clusterCount'], self.sample_cnt,
             delta))
-        for analytic in ['ID', 'SI', 'AD', 'AH', 'AM', 'AW']:
+        print("Message: {}".format(results['message']))
+        for analytic in ['ID', 'SI', 'RI', 'AD', 'AH', 'AM', 'AW']:
             if analytic == 'AM':
                 analytic_pretty = ','.join("{:.6f}".format(a) for a in results[analytic])
             else:
@@ -60,20 +75,22 @@ class AmberStream:
         if len(neg_ids) > 0:
             print("Root Cause:")
             rc = self.amber.get_root_cause(self.sensor_id, id_list=neg_ids)
-            root_cause_pretty = ['\t' + str(id) + ': ' + ','.join("{:.6f}".format(a) for a in root) for (root, id) in zip(rc, neg_ids)]
+            root_cause_pretty = ['\t' + str(id) + ': ' + ','.join("{:.6f}".format(a) for a in root) for (root, id) in
+                                 zip(rc, neg_ids)]
             for root in root_cause_pretty:
                 print(root)
         self.data = []
-        self.data = []
 
-    def stream_csv(self, csv_file, batch_size=20):
+    def stream_csv(self, csv_file, samples_per_request=5):
         """
         Given a path to a csv file, stream data to Amber in sizes specified by batch_size
         :param csv_file: Path to csv file
-        :param batch_size: Batch size to be used on each request
+        :param samples_per_request: number of samples per streaming request
         :return: None
         """
         # Open csv data file and begin streaming
+
+        batch_size = samples_per_request * self.feature_count
         with open(csv_file, 'r') as f:
             csv_reader = csv.reader(f, delimiter=',')
             self.data = []
@@ -94,6 +111,6 @@ class AmberStream:
 
 
 # Specifying sensor_id will use existing over creating a new one
-# streamer = AmberStream(sensor_id='c76b4cc542f434a7')
-streamer = AmberStream()
-streamer.stream_csv('output_current.csv', batch_size=25)
+streamer = AmberStream(sensor_id=None, feature_count=5, samples_to_buffer=10000, anomaly_history_window=10000,
+                       learning_max_samples=1000000, learning_rate_numerator=10, learning_rate_denominator=10000)
+streamer.stream_csv('output_current.csv', samples_per_request=10)
