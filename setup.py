a694a2860e790964e72e42701869329e860e24a2
Jim Turnquist <jim@boonlogic.com>
2021-11-12 17:06:32 -0600

    Update to version 1.0.21

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='boonamber',
-    version='1.0.20',
+    version='1.0.21',
     author="BoonLogic",
     author_email="amber-support@boonlogic.com",
     packages=['boonamber'],
