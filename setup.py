5e74125fbbdae9659b2f549520c49c4233c191aa
Jim Turnquist <jim@boonlogic.com>
2022-06-18 19:37:25 -0500

    Update to 1.0.36

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='boonamber',
-    version='1.0.35',
+    version='1.0.36',
     author="BoonLogic",
     author_email="amber-support@boonlogic.com",
     packages=['boonamber'],
